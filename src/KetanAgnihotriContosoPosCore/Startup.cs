9b0930f0ecf7cb6565b072b75c3026134e4c174d
Ketan Agnihotri <agnihotriketan@gmail.com>
2017-01-23 12:42:43 +0530

    clean up

--- src/KetanAgnihotriContosoPosCore/Startup.cs
+++ src/KetanAgnihotriContosoPosCore/Startup.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using KetanAgnihotriContosoPosCore.Model;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
